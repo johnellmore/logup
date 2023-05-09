3e8c5b409958537133ba8d03082f9938cd5931e1
John Ellmore <495475+johnellmore@users.noreply.github.com>
2023-05-09 00:29:08 -0500

    close connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,20 +3,20 @@ import { LogStore } from "./logStore.ts";
 
 const LOG_STORE = new LogStore();
 
-function payloadEvent(name: string, data: unknown): string {
-  const eventLine = new TextEncoder().encode(`event: ${name}\n`);
-  const payload = new TextEncoder().encode(`${JSON.stringify(data)}\n`);
-  return `\n${eventLine}${payload}`;
+function payloadEvent(name: string, data: unknown): Uint8Array {
+  return new TextEncoder().encode(
+    `\nevent: ${name}\n${JSON.stringify(data)}\n`
+  );
 }
 
 export async function handleConn(conn: Deno.Conn) {
   const httpConn = Deno.serveHttp(conn);
   for await (const requestEvent of httpConn) {
-    handleReq(requestEvent);
+    await handleReq(requestEvent);
   }
 }
 
-function handleReq(reqEv: Deno.RequestEvent): void {
+async function handleReq(reqEv: Deno.RequestEvent): Promise<void> {
   const [logs, release] = LOG_STORE.listen(new LogFilters());
   const body = new ReadableStream({
     async start(controller) {
@@ -30,9 +30,17 @@ function handleReq(reqEv: Deno.RequestEvent): void {
     },
   });
 
-  reqEv.respondWith(
-    new Response(body, {
-      status: 200,
-    })
-  );
+  try {
+    await reqEv.respondWith(
+      new Response(body, {
+        headers: {
+          "content-type": "text/event-stream",
+        },
+        status: 200,
+      })
+    );
+  } catch (e) {
+    // connection was aborted--no way to detect in Deno besides thrown error
+    throw e;
+  }
 }
