cb3a0bdc32d977c0e5c2f812e313ec55b3ab9bc9
John Ellmore <495475+johnellmore@users.noreply.github.com>
2023-05-23 22:39:03 -0500

    add envelope to backend

--- backend/src/LogStore.ts
+++ backend/src/LogStore.ts
@@ -1,9 +1,22 @@
+import { ulid } from "npm:ulid@2";
 import { LogFilters } from "./LogFilters.ts";
 import { generateMockLogEntry } from "./logMock.ts";
+import { LogEnvelope, LogPayload } from "./types.ts";
 
 export type Log = Record<string, unknown>;
 export type LogGenerator = AsyncGenerator<Log>;
 export type Listener = [LogGenerator, () => void];
+
+function generateMockLogEnvelope(): LogEnvelope {
+  return encloseLogInEnvelope(generateMockLogEntry());
+}
+
+function encloseLogInEnvelope(log: LogPayload): LogEnvelope {
+  return {
+    logId: ulid(),
+    payload: log,
+  };
+}
 
 export class LogStore {
   constructor() {}
@@ -15,8 +28,13 @@ export class LogStore {
     };
     async function* gen() {
       const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+      // TEMP generate mock log stream
+      for (const entry of Array(10).fill(0).map(generateMockLogEnvelope)) {
+        yield entry;
+      }
       while (!cancelFlag) {
-        yield generateMockLogEntry();
+        yield generateMockLogEnvelope();
         await sleep(1000);
       }
     }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,12 @@
+import { serveDir } from "https://deno.land/std@0.186.0/http/file_server.ts";
 import { LogFilters } from "./LogFilters.ts";
-import { LogStore } from "./logStore.ts";
+import { LogStore } from "./LogStore.ts";
 
 const LOG_STORE = new LogStore();
 
 function payloadEvent(name: string, data: unknown): Uint8Array {
   return new TextEncoder().encode(
-    `\nevent: ${name}\n${JSON.stringify(data)}\n`
+    `event: ${name}\ndata: ${JSON.stringify(data)}\n\n`
   );
 }
 
@@ -17,11 +18,23 @@ export async function handleConn(conn: Deno.Conn) {
 }
 
 async function handleReq(reqEv: Deno.RequestEvent): Promise<void> {
+  const url = new URL(reqEv.request.url);
+  if (url.pathname === "/logs") {
+    await handleLogRequest(reqEv);
+  } else {
+    const res = serveDir(reqEv.request, {
+      fsRoot: "./frontend/src",
+    });
+    reqEv.respondWith(res);
+  }
+}
+
+async function handleLogRequest(reqEv: Deno.RequestEvent): Promise<void> {
   const [logs, release] = LOG_STORE.listen(new LogFilters());
   const body = new ReadableStream({
     async start(controller) {
       for await (const log of logs) {
-        const payload = payloadEvent("log", log);
+        const payload = payloadEvent("logs", [log]);
         controller.enqueue(payload);
       }
     },
@@ -41,6 +54,9 @@ async function handleReq(reqEv: Deno.RequestEvent): Promise<void> {
     );
   } catch (e) {
     // connection was aborted--no way to detect in Deno besides thrown error
+    if (e.message.includes("connection closed before message completed")) {
+      return;
+    }
     throw e;
   }
 }
